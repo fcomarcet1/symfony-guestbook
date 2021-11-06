3473a8802599b396c17a8f36ce49694b26660bda
fcomarcet1 <fcomarcet1@gmail.com>
2021-11-06 13:43:09 +0100

    Refactoring ConferenceController

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,46 +13,36 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        $conferences = $conferenceRepository->findAll();
-
-        return new Response($twig->render('conference/index.html.twig', [
+        return new Response($this->twig->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]));
-
-        /*return $this->render('conference/index.html.twig', [
-            'conferences' => $conferences,
-        ]);*/
     }
 
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(
-        Request $request,
-        Environment $twig,
-        Conference $conference,
-        CommentRepository $commentRepository
-    ): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
-       /* $comments = $commentRepository->findBy(
-            ['conference' => $conference],
-            ['createdAt' => 'DESC']
-        );*/
-
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
-        return new Response($twig->render('conference/show.html.twig', [
+        return new Response($this->twig->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
-
 }
