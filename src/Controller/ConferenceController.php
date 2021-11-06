80d20cf0aca52509ab3c5d704d13ed0e636105c6
fcomarcet1 <fcomarcet1@gmail.com>
2021-11-06 13:27:49 +0100

    Add conference show

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +26,26 @@ class ConferenceController extends AbstractController
         /*return $this->render('conference/index.html.twig', [
             'conferences' => $conferences,
         ]);*/
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(
+        Environment $twig,
+        Conference $conference,
+        CommentRepository $commentRepository
+    ): Response
+    {
+        $comments = $commentRepository->findBy(
+            ['conference' => $conference],
+            ['createdAt' => 'DESC']
+        );
+
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $comments,
+        ]));
     }
 
 }
