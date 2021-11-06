2ce59c9991d3aebec70b0070e0be78a013feb740
fcomarcet1 <fcomarcet1@gmail.com>
2021-11-06 14:13:18 +0100

    Configure framework.yaml for storing sessions in DB && create migration

---
+++ migrations/Version20211106130920.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211106130920 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Creates table `sessions`';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS sessions (
+                session_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                session_data BYTEA NOT NULL,
+                session_lifetime INTEGER NOT NULL,
+                session_time INTEGER NOT NULL
+            )
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE sessions');
+    }
+}
