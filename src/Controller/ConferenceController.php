a7e3ef00e2cd5adc3d03333776687ba87cbbe292
fcomarcet1 <fcomarcet1@gmail.com>
2021-11-06 11:50:19 +0100

    Add conference list in ConferenceController@index

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        $conferences = $conferenceRepository->findAll();
+
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+
+        /*return $this->render('conference/index.html.twig', [
+            'conferences' => $conferences,
+        ]);*/
     }
 
 }
