e3154c78688d27c2b7fe7e884f35194c102865e0
fcomarcet1 <fcomarcet1@gmail.com>
2021-11-09 20:20:13 +0100

    Handling a Request in CommentFormType

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,23 +7,32 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ConferenceController extends AbstractController
 {
     private Environment $twig;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
      * @Route("/", name="homepage")
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -34,12 +43,30 @@ class ConferenceController extends AbstractController
 
     /**
      * @Route("/conference/{slug}", name="conference")
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(
+        Request           $request,
+        Conference        $conference,
+        CommentRepository $commentRepository
+    ): Response
     {
-
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', [
+                'slug' => $conference->getSlug()
+            ]);
+        }
 
 
         $offset = max(0, $request->query->getInt('offset', 0));
